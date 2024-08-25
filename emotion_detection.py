dc94e426037374172ffc538b64984a6e8521c841
gooflee <gooflee@yahoo.com>
2024-08-25 17:44:09 -0400

    Change function to JSON

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,5 +1,5 @@
 """ emotion prodiction """
-
+import json
 import requests
 
 def emotion_detector(text_to_analyze):
@@ -9,5 +9,30 @@ def emotion_detector(text_to_analyze):
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     inputObject = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url, json = inputObject, headers = headers)
+    responseJson = response.json()
+      
+    emotionPredictions = responseJson["emotionPredictions"][0]["emotion"]
+    anger_score = emotionPredictions["anger"]
+    disgust_score = emotionPredictions["disgust"]
+    fear_score = emotionPredictions["fear"]
+    joy_score = emotionPredictions["joy"]
+    sadness_score = emotionPredictions["sadness"]
 
-    return response.text
+    emotion_scores = {}
+    emotion_scores['anger'] = anger_score
+    emotion_scores['disgust'] = disgust_score
+    emotion_scores['fear'] = fear_score
+    emotion_scores['joy'] = joy_score
+    emotion_scores['sadness'] = sadness_score
+
+    dominant_emotion_name = ""
+    dominant_emotion_score = -1.0
+    for key in emotion_scores.keys():
+        if emotion_scores[key]> dominant_emotion_score:
+           dominant_emotion_name = key
+           dominant_emotion_score = emotion_scores[key]
+    
+    emotion_scores['dominant_emotion'] = dominant_emotion_name
+
+    
+    return json.dumps(emotion_scores)
