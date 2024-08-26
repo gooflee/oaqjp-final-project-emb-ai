0cbe79f71f00e3416ba45fb5346bb003faa37089
gooflee <gooflee@yahoo.com>
2024-08-25 20:49:00 -0400

    completed

--- server.py
+++ server.py
@@ -1,13 +1,15 @@
+""" Server code for emotion detection """
+
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask("Emotion Detection")
 
 @app.route("/emotionDetector")
-def emotionDetector():
+def emotion_detector_details():
     """ recieve text to process and send it to watson and return formated string """
 
-    text_to_analyze = request.args.get("textToAnalyze")     
+    text_to_analyze = request.args.get("textToAnalyze")
     emotions = emotion_detector(text_to_analyze)
     dominant_emotion = emotions["dominant_emotion"]
 
@@ -16,9 +18,9 @@ def emotionDetector():
 
     del emotions["dominant_emotion"]
     formated_emotions = ""
-    for key in emotions.keys():
-        formated_emotions += "\'" + key + "\': " +  str(emotions[key]) + ", "
-    
+    for emotion, score  in emotions:
+        formated_emotions += "\'" + emotion + "\': " +  str(score) + ", "
+
     formated_emotions = formated_emotions[:-2] + ".  "
     message = f'For the given statement, the system response is {formated_emotions}'
     message = message + f'The dominant emotion is <B>{dominant_emotion}</B>'
@@ -30,7 +32,6 @@ def render_index_page():
     """ Loads index.hmtl """
 
     return render_template("index.html")
-
 
 if __name__ == "__main__":
     app.run()
